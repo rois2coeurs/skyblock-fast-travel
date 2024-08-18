3238fd94104de5a17643144e7b0449c819dde413
rois2coeurs <rois2coeurs@gmail.com>
2024-08-18 16:07:49 +0200

    Initial commit

---
+++ src/client/java/com/rois2coeurs/fasttravel/FasttravelClient.java
@@ -0,0 +1,10 @@
+package com.rois2coeurs.fasttravel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FasttravelClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
